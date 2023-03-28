d2e1f2b3967f63686e4b48690b49b5fbb3d18500
Eren Can Bilgen <115940215+ErenBilgenn@users.noreply.github.com>
2023-03-28 15:18:47 +0300

    added HMW3

--- HMW3.sql
+++ HMW3.sql
@@ -1,12 +1,19 @@
+-- List the country names in the country column of the country table, starting with the 'A' character and ending with the 'a' character.
+
+SELECT country_names FROM country
+WHERE country_names LIKE 'A%a' ;
+
+-- List the country names in the country column of the country table, consisting of at least 6 characters and ending with the 'n' character.
 
 SELECT country FROM country
-WHERE country LIKE 'A%a';
+WHERE country LIKE '_____%n' ;
 
-SELECT country FROM country
-WHERE country LIKE '_____%n';
+-- In the title column of the movie table, list the movie names containing at least 4 'T' characters, regardless of upper or lower case letters.
 
 SELECT title FROM film
-WHERE title ILIKE '%t%t%t%t';
+WHERE title ILIKE '%t%t%t%t%' ;
+
+-- From the data in all the columns in the movie table, sort the data that starts with the title 'C' character, has a length greater than 90 and a rental_rate of 2.99.
 
 SELECT * FROM film
-WHERE title LIKE 'C%' AND length >90 AND rental_rate = 2.99;
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99 ;
